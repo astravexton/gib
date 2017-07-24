84477eb4009d1aa6b2d6d71b1cfef9a8094ddb87
nathan <nathan@zyr.io>
2017-07-24 20:59:00 +0100

    Add ServerPassword

--- ircstructs/ircstructs.go
+++ ircstructs/ircstructs.go
@@ -2,15 +2,16 @@ package ircstructs
 
 // Config holds bot config
 type Config struct {
-	Server       string
-	SSL          bool
-	Nick         string
-	Gecos        string
-	Sasl         bool
-	User         string
-	SaslUsername string
-	SaslPassword string
-	Autojoin     string
-	Debug        bool
-	Prefix       string
+	Server         string `json:"server,omitempty"`
+	SSL            bool   `json:"ssl,omitempty"`
+	Nick           string `json:"nick,omitempty"`
+	Gecos          string `json:"gecos,omitempty"`
+	Sasl           bool   `json:"sasl,omitempty"`
+	User           string `json:"user,omitempty"`
+	SaslUsername   string `json:"sasl_username,omitempty"`
+	SaslPassword   string `json:"sasl_password,omitempty"`
+	Autojoin       string `json:"autojoin,omitempty"`
+	Debug          bool   `json:"debug,omitempty"`
+	Prefix         string `json:"prefix,omitempty"`
+	ServerPassword string `json:"server_password,omitempty"`
 }

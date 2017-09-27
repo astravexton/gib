c85e676c09db54a39c5762bc1f7e99d466674c8c
nathan <nathan@zyr.io>
2017-09-27 15:07:12 +0100

    Added more commands

--- bot.go
+++ bot.go
@@ -1,25 +1,47 @@
 package main
 
 import (
-	"bufio"
 	"crypto/tls"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"regexp"
 	"strings"
 	"time"
+	"unicode"
 
-	"github.com/muesli/cache2go"
+	"golang.org/x/tools/imports"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/nanobox-io/scribble"
+	"github.com/shkh/lastfm-go/lastfm"
 	"github.com/thoj/go-ircevent"
 
 	"Gib/ircstructs"
-	"os/exec"
 )
 
-func stringInSlice(a string, list []interface{}) bool {
+func percentCaps(s string) int {
+	var u float64
+
+	re := regexp.MustCompile(`\W`)
+	s = re.ReplaceAllString(s, "")
+
+	for _, a := range s {
+		if unicode.IsUpper(a) {
+			u = u + 1
+		}
+	}
+
+	return int(u / float64(len(s)) * 100)
+}
+
+func stringInSlice(a string, list []string) bool {
 	for _, b := range list {
 		if b == a {
 			return true
@@ -28,44 +50,74 @@ func stringInSlice(a string, list []interface{}) bool {
 	return false
 }
 
+// TimeData holds user timezone
+type TimeData struct {
+	Name     string
+	Timezone string
+}
+
+// Quote holds a user quote
+type Quote struct {
+	Added     int64
+	By        string
+	Quote     string
+	QuoteID   int
+	Upvotes   int
+	Downvotes int
+}
+
 func main() {
 
-	type join struct {
-		nick    string
-		time    time.Time
-		channel string
-	}
+	lastfmapi := lastfm.New("ca347092b54af2ac4cb1f71034c3dd67", "c37e37b32dbe58e2a8cd9959844b8bb6")
 
 	gotemplate := `package main
-
-import (
-	"fmt"
-)
 
 func main() {
 	%s	
 }`
 
-	cache := cache2go.Cache("usercache")
-	cache.SetAddedItemCallback(func(e *cache2go.CacheItem) {
-		fmt.Println("Adding:", e.Key())
-	})
-	cache.SetAboutToDeleteItemCallback(func(e *cache2go.CacheItem) {
-		fmt.Println("Deleting:", e.Key())
-	})
+	done := make(chan struct{})
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter config name (e.g. freenode.json): ")
-	cfile, _ := reader.ReadString('\n')
-	cfile = strings.Split(cfile, "\r")[0]
-
-	content, err := ioutil.ReadFile(cfile)
+	cfile, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	var conf ircstructs.Config
-	err = json.Unmarshal(content, &conf)
-
+	err = json.Unmarshal(cfile, &conf)
 	if err != nil {
-		fmt.Printf("Err %s", err)
+		fmt.Println(err)
+		return
+	}
+
+	db, err := scribble.New(fmt.Sprintf("data/%s/", conf.Name), nil)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	quotedb, err := sql.Open("sqlite3", fmt.Sprintf("data/%s/quotes.db", conf.Name))
+	if err != nil {
+		log.Panic(err)
+	}
+	defer quotedb.Close()
+
+	appingdb, err := sql.Open("sqlite3", fmt.Sprintf("data/%s/apping.db", conf.Name))
+	if err != nil {
+		log.Panic(err)
+	}
+	defer appingdb.Close()
+
+	sqlStmt := `create table if not exists fappers (id integer not null primary key, nick text);
+	create table if not exists faps (id integer not null primary key, fapperid integer, starttime timedaye, endtime timedate);`
+	_, err = appingdb.Exec(sqlStmt)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	_, err = quotedb.Exec("create table if not exists quotes (id integer not null primary key, chan text, quote text, addedby string, time datetime);")
+	if err != nil {
+		log.Panic(err)
 	}
 
 	bot := irc.IRC(conf.Nick, conf.User)
@@ -82,35 +134,36 @@ func main() {
 	bot.Password = conf.ServerPassword
 
 	bot.AddCallback("ERROR", func(e *irc.Event) {
-		fmt.Print("ERROR")
 		bot.Disconnect()
+		fmt.Println(e.Message())
+		os.Exit(1)
 	})
 
 	bot.AddCallback("001", func(e *irc.Event) {
-		bot.SendRawf("MODE %s %s", conf.Nick, conf.Modes)
+		if conf.Modes != "" {
+			bot.SendRawf("MODE %s %s", conf.Nick, conf.Modes)
+		}
+
 		if conf.Autojoin != "" {
 			bot.Join(conf.Autojoin)
 		}
 	})
 
-	bot.AddCallback("JOIN", func(e *irc.Event) {
-		if e.Nick == conf.Nick {
-			return
-		}
-		target := e.Arguments[0]
-		j := join{e.Nick, time.Now(), target}
-		cache.Add(e.Nick, 5*time.Second, &j)
-	})
-
 	bot.AddCallback("PRIVMSG", func(e *irc.Event) {
 		target := e.Arguments[0]
 
-		if strings.Split(e.Message(), " ")[0] == ".choose" {
+		// if percentCaps(e.Message()) >= 80 && len(e.Message()) >= 10 {
+		// 	bot.Kick(e.Nick, target, "Goodbye")
+		// 	return
+		// }
+
+		if strings.Split(e.Message(), " ")[0] == ".choose" && conf.Name == "subluminal" {
 			choices := strings.Split(strings.Split(e.Message(), " ")[1], ",")
 			bot.Privmsgf(target, "%s: %s", e.Nick, choices[rand.Intn(len(choices))])
+			return
 		}
 
-		trigger := fmt.Sprintf(`^(?:%s)(.*?)(?:$|\s+)(.*)`, conf.Prefix)
+		trigger := fmt.Sprintf(`^(%s)(.*?)(?:$|\s+)(.*)`, conf.Prefix)
 		re := regexp.MustCompile(trigger).FindString(e.Message())
 		if re != "" {
 			s := strings.SplitN(re[1:], " ", 2)
@@ -120,55 +173,157 @@ func main() {
 				args = s[1]
 			}
 
-			// if cmd == "compare" {
-			// 	dmp := diffmatchpatch.New()
-			// 	diffs := dmp.DiffMain(strings.SplitN(args, " ", 2)[0], strings.SplitN(args, " ", 2)[1], false)
-			// 	bot.Privmsg(target, dmp.DiffNormalText(diffs))
-			// 	return
-			// }
+			switch cmd {
+			case "quoteadd", "addquote":
+				if args == "" {
+					bot.Privmsgf(target, "%s: quoteadd <quote>", e.Nick)
+					return
+				}
 
-			if cmd == "ping" {
+				tx, err := quotedb.Begin()
+				if err != nil {
+					bot.Privmsgf(target, "error in Begin: %s", err.Error())
+					return
+				}
+
+				s, err := tx.Prepare("insert into quotes (chan, quote, addedby, time) VALUES (?, ?, ?, ?);")
+				if err != nil {
+					bot.Privmsgf(target, "error in Prepare: %s", err.Error())
+					return
+				}
+				r, err := s.Exec(target, args, e.Nick, time.Now().Unix())
+				if err != nil {
+					bot.Privmsgf(target, "error in Exec: %s", err.Error())
+					return
+				}
+				tx.Commit()
+				lr, _ := r.LastInsertId()
+				bot.Privmsgf(target, "%s: quote #%d added.", e.Nick, lr)
+				return
+
+			case "getquote", "quote":
+				if args == "" {
+					bot.Privmsgf(target, "%s: quote <id>", e.Nick)
+					return
+				}
+				stmt, err := quotedb.Prepare("select quote, addedby, time from quotes where id = ?")
+				if err != nil {
+					bot.Privmsgf(target, "error in Prepare: %s", err.Error())
+					return
+				}
+				defer stmt.Close()
+
+				var addedby string
+				var quote string
+				var time time.Time
+				err = stmt.QueryRow(args).Scan(&quote, &addedby, &time)
+				if err != nil {
+					bot.Privmsgf(target, "No such quote id")
+					return
+				}
+				bot.Privmsgf(target, "Quote #%s: \"%s\" added by %s", args, quote, addedby)
+				return
+
+			case "settime":
+				_, err := time.LoadLocation(args)
+				if err != nil {
+					bot.Privmsgf(target, "%s: %q is not a valid location", e.Nick, args)
+					return
+				}
+
+				t := TimeData{Name: e.Nick, Timezone: args}
+				if err := db.Write("timezones", e.Nick, t); err != nil {
+					bot.Privmsgf(target, "%s: error in adding timezone: %s", e.Nick, err.Error())
+					return
+				}
+				bot.Privmsgf(target, "%s: done.", e.Nick)
+
+			case "time":
+				if args == "" {
+					bot.Privmsgf(target, "%s: time <nick>", e.Nick)
+					return
+				}
+
+				t := TimeData{}
+				if err := db.Read("timezones", args, &t); err != nil {
+					bot.Privmsgf(target, "%s: %s has not set their timezone.", e.Nick, args)
+					return
+				}
+
+				loc, _ := time.LoadLocation(t.Timezone)
+				cTime := time.Now().In(loc).Format("Mon, 2 Jan 2006 15:04:05 -0700 (MST)")
+				bot.Privmsgf(target, "%s: %s's time: %s", e.Nick, t.Name, cTime)
+
+			// case "apping":
+			// 	tx, err := appingdb.Begin()
+			// 	if err != nil {
+			// 		bot.Privmsg(target, err.Error())
+			// 		return
+			// 	}
+
+			// 	stmt, _ := tx.Prepare("select count(*) from fappers where nick = ?")
+			// 	defer stmt.Close()
+			// 	var count int
+			// 	err = stmt.QueryRow(e.Nick).Scan(&count)
+			// 	if err != nil {
+			// 		bot.Privmsg(target, err.Error())
+			// 		return
+			// 	}
+
+			// 	if count == 0 {
+
+			// 	}
+
+			// case "apped":
+			// 	fmt.Println("apped")
+
+			// case "appstats":
+			// 	fmt.Println("appstats")
+
+			case "np":
+				np, err := lastfmapi.User.GetRecentTracks(lastfm.P{"user": args})
+				if err != nil {
+					log.Print(err)
+				}
+				bot.Privmsgf(target, "%s is/was listening to: %s - %s", args, np.Tracks[0].Artist.Name, np.Tracks[0].Name)
+
+			case "ping":
 				bot.Privmsg(target, "Pong!")
-				return
-			}
 
-			if cmd == "source" {
+			case "source":
 				bot.Privmsgf(target, "%s: https://bitbucket.org/nathan93b/Gib", e.Nick)
-				return
-			}
 
-			if stringInSlice(e.Source, conf.Admins) == true {
-				if cmd == "go" {
+			case "go":
+				if stringInSlice(e.Source, conf.Admins) == true {
 					stub := fmt.Sprintf(gotemplate, args)
-					err := ioutil.WriteFile("C:/Temp/stub.go", []byte(stub), 0644)
+					tempfile := filepath.Join(os.TempDir(), "stub.go")
+
+					formatted, err := imports.Process("prog.go", []byte(stub), nil)
+					if err != nil {
+						bot.Privmsg(target, err.Error())
+						return
+					}
+
+					err = ioutil.WriteFile(tempfile, []byte(formatted), 0644)
 					if err != nil {
 						fmt.Print(err)
 						return
 					}
 
-					r := exec.Command("go.exe", "run", "C://Temp//stub.go")
-
+					r := exec.Command("go", "run", tempfile)
 					out, err := r.Output()
 					if err != nil {
-						fmt.Print(err)
+						fmt.Println("Error in running:", err)
+						return
 					}
-					bot.Privmsg(target, string(out))
+					fmt.Println(string(out))
+					for _, line := range strings.Split(string(out), "\n") {
+						bot.Privmsg(target, line)
+					}
 					return
 				}
 			}
 		}
-
-		// leave this till last
-
-		// res, err := cache.Value(e.Nick)
-		// if err != nil {
-		// 	return
-		// }
-
-		// if time.Now().Unix() <= res.Data().(*join).time.Add(3*time.Second).Unix() {
-		// 	cache.Delete(e.Nick)
-		// 	bot.Kick(e.Nick, target, "Goodbye")
-		// }
 
 	})
 
@@ -179,5 +334,13 @@ func main() {
 		return
 	}
 
-	bot.Loop()
+	go bot.Loop()
+
+	done <- struct{}{}
+	<-done
+
+}
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
 }

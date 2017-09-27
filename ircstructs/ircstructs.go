a2e5a75fca457043d1db8259f4efb033c6086e58
nathan <nathan@zyr.io>
2017-09-27 15:06:48 +0100

    Redo ircstructs

--- ircstructs/ircstructs.go
+++ ircstructs/ircstructs.go
@@ -1,19 +1,31 @@
 package ircstructs
 
+// CommandThing holds stuff
+type CommandThing struct {
+	commandHooks map[string][]func(s string) string
+}
+
 // Config holds bot config
 type Config struct {
-	Server         string        `json:"server,omitempty"`
-	SSL            bool          `json:"ssl,omitempty"`
-	Nick           string        `json:"nick,omitempty"`
-	Gecos          string        `json:"gecos,omitempty"`
-	Sasl           bool          `json:"sasl,omitempty"`
-	User           string        `json:"user,omitempty"`
-	SaslUsername   string        `json:"sasl_username,omitempty"`
-	SaslPassword   string        `json:"sasl_password,omitempty"`
-	Autojoin       string        `json:"autojoin,omitempty"`
-	Debug          bool          `json:"debug,omitempty"`
-	Prefix         string        `json:"prefix,omitempty"`
-	ServerPassword string        `json:"server_password,omitempty"`
-	Modes          string        `json:"modes,omitempty"`
-	Admins         []interface{} `json:"admins,omitempty"` // if there is a better way to do this, tell me
+	Name           string   `json:"name"`
+	Active         bool     `json:"active,omitempty"`
+	Server         string   `json:"server,omitempty"`
+	SSL            bool     `json:"ssl,omitempty"`
+	Nick           string   `json:"nick,omitempty"`
+	Gecos          string   `json:"gecos,omitempty"`
+	Sasl           bool     `json:"sasl,omitempty"`
+	User           string   `json:"user,omitempty"`
+	SaslUsername   string   `json:"sasl_username,omitempty"`
+	SaslPassword   string   `json:"sasl_password,omitempty"`
+	Autojoin       string   `json:"autojoin,omitempty"`
+	Debug          bool     `json:"debug,omitempty"`
+	Prefix         string   `json:"prefix,omitempty"`
+	ServerPassword string   `json:"server_password,omitempty"`
+	Modes          string   `json:"modes,omitempty"`
+	Admins         []string `json:"admins,omitempty"`
+}
+
+// Servers holds the array of servers
+type Servers struct {
+	Server []Config `json:"servers"`
 }
